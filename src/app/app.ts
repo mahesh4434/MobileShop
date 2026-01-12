e9e3adc6b8bf5a6c66c02d1edc3999b673f1ac7a
mahesh4434 <maheshchaudhari511@gmail.com>
2026-01-12 23:55:31 +0530

    Router Added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Inventory } from './pages/inventory/inventory';
 import { Customer } from './pages/customer/customer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
