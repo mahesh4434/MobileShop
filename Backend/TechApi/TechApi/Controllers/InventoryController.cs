3cba4c003fb1b6e02e910721e379f8ab81b7359a
mahesh4434 <maheshchaudhari511@gmail.com>
2026-01-14 16:24:45 +0530

    Backend development started

---
+++ Backend/TechApi/TechApi/Controllers/InventoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+
+        public ActionResult SaveInventoryData() {             // Placeholder for saving inventory data logic
+
+
+            return Ok("Inventory data saved successfully.");
+        }
+    }
+}
