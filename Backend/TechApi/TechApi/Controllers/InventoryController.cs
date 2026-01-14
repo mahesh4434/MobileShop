bdfcf17e0d3734f9e80546f74ba72d0d09ec6349
mahesh4434 <maheshchaudhari511@gmail.com>
2026-01-14 23:59:24 +0530

    API Running SUCESSFULLY

--- Backend/TechApi/TechApi/Controllers/InventoryController.cs
+++ Backend/TechApi/TechApi/Controllers/InventoryController.cs
@@ -1,17 +1,36 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace TechApi.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class InventoryController : ControllerBase
-    {
-
-        public ActionResult SaveInventoryData() {             // Placeholder for saving inventory data logic
+  using Microsoft.AspNetCore.Mvc;
+  using Microsoft.Data.SqlClient;
+  using TechApi.Models;
 
 
-            return Ok("Inventory data saved successfully.");
-        }
-    }
-}
+  namespace TechApi.Controllers
+  {
+      [Route("api/[controller]")]
+      [ApiController]
+      public class InventoryController : ControllerBase
+      {
+      [HttpPost]
+          public ActionResult SaveInventoryData(Inventory inventoryDto) {
+        SqlConnection connection = new SqlConnection
+        {
+          ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=techDb;"
+        };
+
+        SqlCommand command = new SqlCommand
+        {
+          CommandText = "sp_SaveInventoryData",
+          CommandType = System.Data.CommandType.StoredProcedure,
+          Connection = connection
+
+        };
+        command.Parameters.AddWithValue("@ProductId", inventoryDto.ProductId);
+        command.Parameters.AddWithValue("@ProductName", inventoryDto.ProductName);
+        command.Parameters.AddWithValue("@StockAvailable", inventoryDto.StockAvailable);
+        command.Parameters.AddWithValue("@ReorderStock", inventoryDto.ReorderStock);
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
+        return Ok("Inventory data saved successfully.");
+          }
+      }
+  }
