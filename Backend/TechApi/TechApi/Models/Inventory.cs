78252208a64ab14867301c70dc303f93e66ad8c1
mahesh4434 <maheshchaudhari511@gmail.com>
2026-01-16 00:04:36 +0530

    Ng module added

--- Backend/TechApi/TechApi/Models/Inventory.cs
+++ Backend/TechApi/TechApi/Models/Inventory.cs
@@ -2,11 +2,11 @@ namespace TechApi.Models
 {
   public class Inventory
   {
-    #region Properties
-    public int ProductId { get; set; }
-    public string ProductName { get; set; }
-    public int StockAvailable { get; set; }
-    public int ReorderStock { get; set; }
-    #endregion
-  }
+        #region Properties
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public int StockAvailable { get; set; }
+        public int ReorderStock { get; set; }
+        #endregion
+    }
 }
